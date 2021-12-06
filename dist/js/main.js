2c55c815d7aebe06834818747dd507b43f4ad58c
ASIFIWE Emmanuel <emmanuel2koestner@gmail.com>
2021-12-06 18:24:06 +0200

    additional of other pages to complete dashboard for user

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,15 @@
 const toggleBtn = document.getElementById('btn-deactive');
 const navContainer = document.querySelector('.header__nav--container');
+const cardList = document.querySelectorAll('#dashboard-card');
+
+console.log(cardList);
 
 
-const addActive = (container, img) => {
+const addActive = (container, img= '') => {
     container.classList.add('active');
     img.src = "./dist/img/close.svg";
 }
-const removeActive = (container, img) => {
+const removeActive = (container, img= '') => {
     container.classList.remove('active');
     img.src = "./dist/img/bars.svg";
 }
@@ -20,3 +23,49 @@ toggleBtn.addEventListener('click', () => {
         addActive(navContainer, img);
     }
 });
+
+cardList.forEach(card => {
+    const deleteBtn = card.querySelector('.btn__delete');
+    const cardConfirm = card.querySelector('.card__confirm-delete');
+    deleteBtn.addEventListener('click', () => {
+        console.log(card);
+        cardConfirm.classList.add('active');
+    });
+    const cancelBtn = cardConfirm.querySelector('.btn__create');
+    cancelBtn.addEventListener('click', () => {
+        cardConfirm.classList.remove('active');
+    })
+    const deleteConfirmBtn = cardConfirm.querySelector('.btn__delete');
+    deleteConfirmBtn.addEventListener('click', () => {
+        card.classList.add('remove');
+    })
+})
+
+const inpFile = document.getElementById('inptFile');
+const previewContainer = document.getElementById('imagePreview');
+const previewImage = previewContainer.querySelector('.image-preview__image');
+const previewText = previewContainer.querySelector('.image-preview__default-text');
+
+inpFile.addEventListener('change', function(){
+    const file = this.files[0];
+    
+    if (file) {
+        const reader = new FileReader();
+
+        previewText.style.display = "none";
+        previewImage.style.display = "block";
+
+        reader.addEventListener('load', function() {
+            console.log(this);
+            previewImage.setAttribute("src", this.result);
+        });
+
+        reader.readAsDataURL(file);
+    } else {
+        // console.log('junior');
+        previewText.style.display = null;
+        previewImage.style.display = null;
+
+        previewImage.setAttribute("src", "");
+    }
+})
