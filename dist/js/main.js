ec47ca57d84e58da13b17049e650cf524142ec50
ASIFIWE Emmanuel <emmanuel2koestner@gmail.com>
2021-12-13 16:25:53 -0800

    improving interaction using js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,8 @@
 const toggleBtn = document.getElementById('btn-deactive');
 const navContainer = document.querySelector('.header__nav--container');
 const cardList = document.querySelectorAll('#dashboard-card');
+const infoReadMoreBtn = document.getElementById('info-readmore');
+const modalContainer = document.querySelector('.about--modal');
 
 console.log(cardList);
 
@@ -41,31 +43,41 @@ cardList.forEach(card => {
     })
 })
 
-const inpFile = document.getElementById('inptFile');
-const previewContainer = document.getElementById('imagePreview');
-const previewImage = previewContainer.querySelector('.image-preview__image');
-const previewText = previewContainer.querySelector('.image-preview__default-text');
+// // const inpFile = document.getElementById('inptFile');
+// const inpFile = document.querySelector('.inptFile');
+// const previewContainer = document.getElementById('imagePreview');
+// const previewImage = document.querySelector('.image-preview__image');
+// const previewText = document.querySelector('.image-preview__default-text');
 
-inpFile.addEventListener('change', function(){
-    const file = this.files[0];
+// inpFile.addEventListener('change', function(){
+//     const file = this.files[0];
     
-    if (file) {
-        const reader = new FileReader();
+//     if (file) {
+//         const reader = new FileReader();
 
-        previewText.style.display = "none";
-        previewImage.style.display = "block";
+//         previewText.style.display = "none";
+//         previewImage.style.display = "block";
 
-        reader.addEventListener('load', function() {
-            console.log(this);
-            previewImage.setAttribute("src", this.result);
-        });
+//         reader.addEventListener('load', function() {
+//             console.log(this);
+//             previewImage.setAttribute("src", this.result);
+//         });
 
-        reader.readAsDataURL(file);
-    } else {
-        // console.log('junior');
-        previewText.style.display = null;
-        previewImage.style.display = null;
+//         reader.readAsDataURL(file);
+//     } else {
+//         // console.log('junior');
+//         previewText.style.display = null;
+//         previewImage.style.display = null;
 
-        previewImage.setAttribute("src", "");
-    }
+//         previewImage.setAttribute("src", "");
+//     }
+// })
+
+infoReadMoreBtn.addEventListener('click', () => {
+    modalContainer.classList.add('active');
+
+})
+const closeBtn = document.getElementById('btn-deactive--modal')
+closeBtn.addEventListener('click', () => {
+    modalContainer.classList.remove('active');
 })
